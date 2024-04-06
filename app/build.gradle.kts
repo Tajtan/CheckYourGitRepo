1510843459016fb3b7c3510982d55d9011809b68
Tomasz Łabiński <tomeklab@gmail.com>
2024-04-06 18:49:57 +0200

    Adding Dependencies for ViewModel, Network Calls and JSON

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,14 @@ android {
 
 dependencies {
 
+    // Adding my dependencies
+    // Compose ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    // Network calls
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    //Json to Kotlin object mapping
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
